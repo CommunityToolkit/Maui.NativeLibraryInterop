9c9930d01f436abed1116396e46aa1233b9cb2c3
Peter Collins <pecolli@microsoft.com>
2024-04-26 14:34:56 -0700

    [googlecast] Fix warnings in the sample app
    The google cast sample has been updated to resolve all build warnings
    and improve formatting.

---
+++ googlecast/macios/README.md
@@ -0,0 +1,12 @@
+# Google Cast Slim Binding
+This folder contains a slim binding for the Google Cast SDK.
+
+### Build and Run
+```shell
+    dotnet build sample -t:Run
+```
+
+### Configure
+For more details, reference the [Get Started][0] page.
+
+[0]: https://developers.google.com/cast
--- googlecast/macios/sample/MainPage.xaml.cs
+++ googlecast/macios/sample/MainPage.xaml.cs
@@ -2,16 +2,14 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
 	public MainPage()
 	{
 		InitializeComponent();	
 	}
 
-    protected override async void OnAppearing()
-    {
-        base.OnAppearing();
+	protected override async void OnAppearing()
+	{
+		base.OnAppearing();
 
 		if (googleCastManager is null)
 		{
@@ -22,13 +20,13 @@ public partial class MainPage : ContentPage
 			await Permissions.RequestAsync<Permissions.NetworkState>();
 			#endif
 		}
-    }
+	}
 
-    GoogleCast.GoogleCastManager googleCastManager;
+	GoogleCast.GoogleCastManager? googleCastManager;
 
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
-		googleCastManager.LoadMedia("https://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4",
+		googleCastManager?.LoadMedia("https://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4",
 			"video/mp4",
 			"Big Buck Bunny",
 			"Big Buck Bunny (open-source movie)",
