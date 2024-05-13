b06188fa454ce995f74b3d866672b04a86a4e2db
Peter Collins <pecolli@microsoft.com>
2024-05-13 09:53:12 -0700

    [android] Add gradle build task (#19)
    Introduces a new `@(GradleProjectReference)` item to help simplify the
    content that needs to be added to the binding .csproj file.  This item
    will translate relevant metadata to the `@(AndroidLibrary)` item that is
    automatically added to the project after the gradle project is built.
    
    A gradle build task has been added to improve msbuild output and error
    logging when a gradle invocation fails.
    
    We will now attempt to set `$ANDROID_HOME` and `$JAVA_HOME`
    automatically when invoking gradle.  These values will be determined by
    Android SDK tooling resolution tasks, and can be changed by setting
    `$(AndroidSdkDirectory)` and `$(JavaSdkDirectory)` in the project file or
    on the command line.

---
+++ eng/src/Microsoft.Maui.BindingExtensions.Build.Tasks/Tasks/Gradle.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Build.Framework;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.Maui.BindingExtensions.Build.Tasks
+{
+    public class Gradle : BindingToolTask
+    {
+        public override string TaskPrefix => "GDL";
+
+        protected override string ToolName => RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "gradlew.bat" : "gradlew";
+
+
+        public string AndroidSdkDirectory { get; set; } = string.Empty;
+
+        public string JavaSdkDirectory { get; set; } = string.Empty;
+
+        public string Arguments { get; set; } = string.Empty;
+
+
+        public Gradle()
+        {
+        }
+
+        protected override string GenerateFullPathToTool()
+        {
+            return Path.Combine(ToolPath, ToolExe);
+        }
+
+        protected override string GenerateCommandLineCommands() => Arguments;
+
+        protected override ProcessStartInfo GetProcessStartInfo(string pathToTool, string commandLineCommands, string responseFileSwitch)
+        {
+            ProcessStartInfo psi = base.GetProcessStartInfo(pathToTool, commandLineCommands, responseFileSwitch);
+            if (Directory.Exists(AndroidSdkDirectory))
+                psi.Environment["ANDROID_HOME"] = AndroidSdkDirectory;
+
+            if (Directory.Exists(JavaSdkDirectory))
+                psi.Environment["JAVA_HOME"] = JavaSdkDirectory;
+
+            return psi;
+        }
+
+    }
+}
