06242243ee4262a32e0dd753441a890fc2cf932f
Peter Collins <pecolli@microsoft.com>
2024-06-19 13:01:04 -0400

    [buildtasks] Add classic XI install check to sharpie

---
+++ src/CommunityToolkit.Maui.BindingExtensions.Tests/SharpieTaskTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using CommunityToolkit.Maui.BindingExtensions;
+using NUnit.Framework;
+
+namespace CommunityToolkit.Maui.BindingExtensions.Tests
+{
+    [TestFixture]
+    public class SharpieTaskTests
+    {
+        MockBuildEngine engine = new MockBuildEngine();
+
+        [SetUp]
+        public void Setup()
+        {
+            engine.ClearEvents();
+        }
+
+        [Test]
+        public void ShouldRunSuccessfully()
+        {
+            var task = new Sharpie()
+            {
+                BuildEngine = engine,
+                Arguments = "xcode -sdks",
+            };
+
+            var taskSucceeded = task.Execute();
+            taskSucceeded.Should().Be(true, "Task should complete successfully.");
+            task.ConsoleOutput.Should().Contain("iphoneos", "Sharpie 'xcode -sdks' output should contain at least one 'iphoneos' entry.");
+        }
+    }
+}
